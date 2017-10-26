f2d56cf28c935b17b406f964a1d9327cc39153eb
donovonjenson <donovonjenson@hotmail.com>
2017-10-25 18:25:56 -0700

    Did most of everything

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,61 @@
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      video: exampleVideoData[0],
+      videos: exampleVideoData
+    };
+
+    this.onSearch('');
+  }
+
+  onTitleClick(video) {
+    this.setState({video: video});
+  }
+
+  onSearch(term) {
+    var app = this;
+    searchYouTube({
+      key: window.YOUTUBE_API_KEY,
+      q: term, 
+      maxResults: '5', 
+      type: 'video',
+      videoEmbeddable: 'true',
+      part: 'snippet'
+    }, 
+      function(data) {
+        console.log(data);
+        app.setState({
+          videos: data.items,
+          video: data.items[0]
+        });
+      } 
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <Search searchClick={this.onSearch.bind(this)}/>
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <VideoPlayer video={this.state.video} />
+          </div>
+          <div className="col-md-5">
+            <VideoList videos={this.state.videos} clickListener={this.onTitleClick.bind(this)} />
+          </div>
+        </div>
       </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
-      </div>
-      <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,20 @@
 var searchYouTube = (options, callback) => {
   // TODO
+  $.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    type: 'GET',
+    data: options,
+    success: function(data) {
+      callback(data);
+    }
+  });
+  // fetch('https://www.googleapis.com/youtube/v3/search', { data: options}).then(
+  //   function(response) {
+  //     if (response.ok) {
+  //       console.log(response.blob());
+  //       return response.blob();
+  //     }
+  //   });
 };
 
 window.searchYouTube = searchYouTube;
